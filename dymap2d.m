64d76a7dafa32b68aa03864748493197d4589257
Paolo Macias <Paolo Macias>
2017-01-26 23:39:36 -0700

    Improved code readability, simplified input parameters

--- dymap2d.m
+++ dymap2d.m
@@ -1,16 +1,16 @@
-function [xpoints,ypoints] = dymap2d (rho,gamma,x0,y0,stepsize,maxVal)
+function [xpoints,ypoints] = dymap2d (rho,gamma,x0,y0,t)
 
-xpoints = x0:stepsize:maxVal;
-ypoints = y0:stepsize:maxVal;
+xpoints = x0:t;
+ypoints = y0:t;
 
-for i=2:size(xpoints,2)
-    ypoints(i) = xpoints(i-1);
-    xpoints(i) = rho-((xpoints(i-1))^2) + (gamma*ypoints(i-1));
+for i=1:size(xpoints,2)
+    ypoints(i+1) = xpoints(i);
+    xpoints(i+1) = (rho-(xpoints(i)^2)) + (gamma*ypoints(i));
 end
 
 plot(xpoints)
 yyaxis right
-plot(ypoints,'--m')
+plot(ypoints,'--')
     
 
 
