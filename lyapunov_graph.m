15f8c3422c0eeb0f374df6e68827a99bc35664dd
Paolo Macias <Paolo Macias>
2017-02-05 13:49:02 -0700

    added script to create figure for lyanpov (fig 4)

--- lyapunov_graph.m
+++ lyapunov_graph.m
@@ -1,11 +1,11 @@
-F = @(xy,rho,gamma) [(rho-(xy(1)^2)) + (gamma*xy(2)),xy(2)];
-F_Jacobian =@(xy,rho,gamma) [0 1; 2*xy(1) gamma];
+F = @(xy,rho,gamma) [(rho-(xy(1)^2)) + (gamma*xy(2));xy(1)];
+F_Jacobian =@(xy,rho,gamma) [-2*xy(1) gamma;1 0];
 
 max_time = 500;
-parameter1_range = 2:0.001:3;
-parameter2 = 0.2;
-x0 = 0.5;
-y0 = 0.5;
+parameter1_range = 0:0.001:1.5;
+parameter2 = 0.3;
+x0 = 0.1;
+y0 = 0.1;
 rho = 1.2;
 gamma = 0.3;
 
