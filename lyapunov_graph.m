926b2d0f76c1d611b56932f64c40ba5dc4d77198
Unknown <wcollishaw@unm.edu>
2017-02-05 19:16:20 -0700

    Added code to sum the lyapunov eponents
    The sum of the lyapunov exponents determines if a system is dissipative or conservative

--- lyapunov_graph.m
+++ lyapunov_graph.m
@@ -11,6 +11,17 @@ gamma = 0.3;
 
 max_lyapunovs = lyapunov2d(F, F_Jacobian, max_time, parameter1_range, parameter2, x0, y0);
 
+%Summing the lyapunov exponents to see if the system is 
+%dissipative or conservative
+lyapunovSum = nansum(max_lyapunovs);
+if lyapunovSum < 0
+    lyapunovSum
+    disp('System is dissipative')
+end
+if lyapunovSum == 0
+    lyapunovSum
+    disp('System is conservative')
+end
 plot(parameter1_range,max_lyapunovs, parameter1_range, 0, 'k.')
 xlabel('Param1: a','FontSize', 24);
 ylabel('\lambda_{max}','FontSize', 24);
