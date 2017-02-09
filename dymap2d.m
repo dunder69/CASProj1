885c5c9f0707ad5197ad584caa18ae23f08c2524
Paolo Macias <Paolo Macias>
2017-02-09 15:06:45 -0700

    Updated figures in part two with a smaller, more interesting delta

--- dymap2d.m
+++ dymap2d.m
@@ -8,18 +8,18 @@ for i=1:size(xpoints,2)
     xpoints(i+1) = (rho-(xpoints(i)^2)) + (gamma*ypoints(i));
 end
 
-plot(xpoints)
-xlabel('t')
-ylabel('x_t Values')
-yyaxis right
-plot(ypoints,'--m')
-ylabel('y_t Values')
+% plot(xpoints)
+% xlabel('t')
+% ylabel('x_t Values')
+% yyaxis right
+% plot(ypoints,'--m')
+% ylabel('y_t Values')
 
-figure;
-plot(xpoints,ypoints,'.')
-title('Attractors')
-xlabel('xpoints')
-ylabel('ypoints')
+% figure;
+% quiver(xpoints,ypoints,gradient(xpoints),gradient(ypoints))
+% title('Attractors')
+% xlabel('xpoints')
+% ylabel('ypoints')
 
     
 
--- fig2ab.m
+++ fig2ab.m
@@ -2,14 +2,18 @@ rho = 1.29;
 gamma = 0.3;
 x0 = rand;
 y0 = rand;
-delta = .01;
+delta = .001;
 max =1;
 
 [xpoints,ypoints] = dymap2d(rho,gamma,x0,y0,50);
 [xpoints2,ypoints2] = dymap2d(rho,gamma,x0+delta,y0,50);
 
-plot(xpoints);
-plot(xpoints2,'--m');
+plot(xpoints,'-s','MarkerFaceColor','b','MarkerSize',2);
+hold on
+plot(xpoints2,'--sm','MarkerFaceColor','m','MarkerSize',2);
+title('Time Series with Slightly Varied Initial Conditions');
+xlabel('t','FontSize', 14);
+ylabel('x values','FontSize', 14);
 
 divTimes = zeros(100,2);
 threshold = 0.1;
@@ -18,9 +22,12 @@ t = 200;
 for i=1:100
     divTimes(i,1) = delta;
     divTimes(i,2) = dymapdiff(rho,gamma,x0,y0,t, delta, threshold);
-    delta = delta + 0.01;
+    delta = delta + 0.001;
 end
 
 figure;
 plot(divTimes(:,1),divTimes(:,2));
 axis([0 inf 0 inf]);
+title('Trajectory Divergence  as a Function of the Diffference in Initial Conditions');
+xlabel('Difference in Rho Value','FontSize', 11);
+ylabel('Iterations Until Divergence with Delta = 0.001','FontSize', 11);
