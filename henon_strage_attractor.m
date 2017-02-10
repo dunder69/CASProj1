8846dd15e3fd9fcf0351ab2c636321056fc3be35
Paolo Macias <Paolo Macias>
2017-02-09 17:13:19 -0700

    Added plot of 50000 system 1 points in space full scale and zoomed in, to show cantor set

---
+++ henon_strage_attractor.m
@@ -0,0 +1,6 @@
+[xpoints,ypoints] = dymap2d (1.415,.3,.1,.1,50000);
+plot(xpoints,ypoints,'.')
+
+title('System 1 Plotted in Space')
+xlabel('xpoints','FontSize', 11)
+ylabel('ypoints','FontSize', 11)
