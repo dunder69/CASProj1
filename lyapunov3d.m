61f44bebb9557d4042acf4c3f4c3c05db982a0d7
Tomyao <Tomyao.l@gmail.com>
2017-02-09 23:35:19 -0700

    better lyapunov for part 2

--- lyapunov3d.m
+++ lyapunov3d.m
@@ -1,4 +1,4 @@
-function lyapunov_exp = lyapunov3d(mygamma)
+function [lyapunov_exp,lyapunov_exp_sum] = lyapunov3d(mygamma)
 
 x0 = 0.1;
 y0 = 0.1;
@@ -8,8 +8,8 @@ alpha = 0.2;
 beta = 0.2;
 gamma = mygamma;
 t_start = 0;
-t_transient = 3000;
-t_end = 3100;
+t_transient = 100000;
+t_end = 100100;
 
 t = t_start;
 x = x0;
@@ -17,11 +17,12 @@ y = y0;
 z = z0;
 
 lyapunov_exp = 0;
+%lyapunov_expx = 0;
+%lyapunov_expy = 0;
+%lyapunov_expz = 0;
+lyapunov_exp_sum = 0;
 
 while t <= t_end
-    xprev = x;
-    yprev = y;
-    zprev = z;
     deltax = (x*y - x*gamma + alpha);
     deltay = (-1*(z + x));
     deltaz = (beta*z + y);
@@ -30,8 +31,24 @@ while t <= t_end
     z = z + delta*deltaz;
     if t > t_transient
         lyapunov_exp = lyapunov_exp + log(sqrt(deltax^2 + deltay^2 + deltaz^2));
+        %lyapunov_expx = lyapunov_expx + deltax;
+        %lyapunov_expy = lyapunov_expy + deltay;
+        %lyapunov_expz = lyapunov_expz + deltaz;
+        lyapunov_exp_sum = lyapunov_exp_sum + deltax + deltay + deltaz;
     end
     t = t + delta;
 end
 
 lyapunov_exp = lyapunov_exp/(t_end - t_transient);
+%lyapunov_expx = lyapunov_expx/(t_end - t_transient);
+%lyapunov_expy = lyapunov_expy/(t_end - t_transient);
+%lyapunov_expz = lyapunov_expz/(t_end - t_transient);
+lyapunov_exp_sum = lyapunov_exp_sum/(t_end - t_transient);
+
+if gamma < 5.701 && gamma > 5.699
+    gamma
+    lyapunov_exp_sum
+end
+
+end
+
--- lyapunov3dgraph.m
+++ lyapunov3dgraph.m
@@ -1,18 +1,34 @@
-gammastart = 0;
-gammaend = 30;
+gammastart = 3;
+gammaend = 6;
 gammadelta = 0.01;
-gamma_range = gammastart:gammadelta:(gammaend - gammadelta);
+gamma_range = gammastart:gammadelta:gammaend;
 
 gamma = gammastart;
 index = 0;
 
 while gamma < gammaend
     index = index + 1;
-    lyapunov_exp(index) = lyapunov3d(gamma);
+    [lyapunov_exp(index),lyapunov_exp_sum(index)] = lyapunov3d(gamma);
     gamma = gamma + gammadelta;
 end
 
-plot(gamma_range,lyapunov_exp, gamma_range, 0, 'k.')
+%plot(gamma_range,lyapunov_expx,'--r')
+%hold on
+%plot(gamma_range,lyapunov_expy, ':b')
+%hold on
+%plot(gamma_range,lyapunov_expz, '-g')
+%xlabel('Param1: gamma','FontSize', 24);
+%ylabel('\lambda_{max}','FontSize', 24);
+%title('System 2 Lyapunov Exponents');
+
+figure;
+plot(gamma_range,lyapunov_exp, '-b')
 xlabel('Param1: gamma','FontSize', 24);
-ylabel('\lambda_{max}','FontSize', 24);
+ylabel('\lambda','FontSize', 24);
 title('System 2 Lyapunov Exponents');
+
+figure;
+plot(gamma_range,lyapunov_exp_sum, '-b')
+xlabel('Param1: gamma','FontSize', 24);
+ylabel('\lambda sum','FontSize', 24);
+title('System 2 Lyapunov Exponent Sums');
